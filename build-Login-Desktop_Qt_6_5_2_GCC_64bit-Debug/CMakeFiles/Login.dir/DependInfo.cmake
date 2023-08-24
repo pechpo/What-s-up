c89cb7c75fdbddbd3a1597585197671b019ac637
chriswangfinale <chriswangfinale@proton.me>
2023-08-24 17:00:15 +0800

    Update login for test

---
+++ build-Login-Desktop_Qt_6_5_2_GCC_64bit-Debug/CMakeFiles/Login.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/chriswyc/build-Login-Desktop_Qt_6_5_2_GCC_64bit-Debug/Login_autogen/mocs_compilation.cpp" "CMakeFiles/Login.dir/Login_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/Login.dir/Login_autogen/mocs_compilation.cpp.o.d"
+  "/home/chriswyc/Login/logindialog.cpp" "CMakeFiles/Login.dir/logindialog.cpp.o" "gcc" "CMakeFiles/Login.dir/logindialog.cpp.o.d"
+  "/home/chriswyc/Login/main.cpp" "CMakeFiles/Login.dir/main.cpp.o" "gcc" "CMakeFiles/Login.dir/main.cpp.o.d"
+  "/home/chriswyc/Login/mainwindow.cpp" "CMakeFiles/Login.dir/mainwindow.cpp.o" "gcc" "CMakeFiles/Login.dir/mainwindow.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
