de50a56e3685fbd7a341fcf4478e85930a816dc5
hzxx066 <hzxx066@163.com>
2023-08-25 15:56:02 +0800

    update test_connection

---
+++ test_connect/test_server/main.cpp
@@ -0,0 +1,27 @@
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QDebug>
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    QTcpServer server;
+
+    // 当有新的连接时创建一个新的 socket
+    QObject::connect(&server, &QTcpServer::newConnection, [&server]() {
+        QTcpSocket *socket = server.nextPendingConnection();
+        qDebug() << "Client connected:" << socket->peerAddress().toString();
+        socket->deleteLater();
+    });
+
+    // 开始侦听端口 10043
+    if (server.listen(QHostAddress::Any, 10043)) {
+        qDebug() << "Server started on port 10043";
+    } else {
+        qDebug() << "Failed to start server:" << server.errorString();
+        return 1;
+    }
+
+    return app.exec();
+}
