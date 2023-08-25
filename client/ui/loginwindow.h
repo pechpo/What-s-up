bb94397cd5b16a6b6aa6a0bc98e8320015a2bca9
DuffyGod <lambert_study@126.com>
2023-08-25 10:43:46 +0800

    测试创建Qt Design类

--- client/ui/loginwindow.h
+++ client/ui/loginwindow.h
@@ -3,14 +3,20 @@
 
 #include <QWidget>
 
+namespace Ui {
+class LoginWindow;
+}
+
 class LoginWindow : public QWidget
 {
     Q_OBJECT
+
 public:
     explicit LoginWindow(QWidget *parent = nullptr);
+    ~LoginWindow();
 
-signals:
-
+private:
+    Ui::LoginWindow *ui;
 };
 
 #endif // LOGINWINDOW_H
