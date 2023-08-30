5425a5476b226ce98c37fc2ccffbfbaa51b4bccd
foreverlasting <573902690@qq.com>
2023-08-30 14:40:59 +0800

    update log?

--- backend/src/include/log.h
+++ backend/src/include/log.h
@@ -1,78 +1,27 @@
-//
-// Created by 李嘉政 on 2023/8/30.
-//
+#ifndef LOG_H
+#define LOG_H
 
-#ifndef BACKEND_LOG_H
-#define BACKEND_LOG_H
+#include <QObject>
 
-#endif //BACKEND_LOG_H
 
-#include <iostream>
-#include <fstream>
+//产生日志
 
-class Log {
+class Log : public QObject {
+Q_OBJECT
+
 public:
-    static Log *get_instance() {
-        static Log instance;
-        return &instance;
-    }
+    explicit Log(QObject *parent = nullptr);
 
-    void log(const std::string &msg) {
-        std::ofstream fout("log.txt", std::ios::app);
-        fout << msg << std::endl;
-        fout.close();
-    }
+    static Log *GetLogObj();
 
-    void log(const char *msg) {
-        std::ofstream fout("log.txt", std::ios::app);
-        fout << msg << std::endl;
-        fout.close();
-    }
+    void writeLog(QString log);
 
-    void log(const int &msg) {
-        std::ofstream fout("log.txt", std::ios::app);
-        fout << msg << std::endl;
-        fout.close();
-    }
+signals:
 
-    void log(const double &msg) {
-        std::ofstream fout("log.txt", std::ios::app);
-        fout << msg << std::endl;
-        fout.close();
-    }
-
-    void log(const bool &msg) {
-        std::ofstream fout("log.txt", std::ios::app);
-        fout << msg << std::endl;
-        fout.close();
-    }
-
-    void log(const char &msg) {
-        std::ofstream fout("log.txt", std::ios::app);
-        fout << msg << std::endl;
-        fout.close();
-    }
-
-    void log(const unsigned char &msg) {
-        std::ofstream fout("log.txt", std::ios::app);
-        fout << msg << std::endl;
-        fout.close();
-    }
-
-    void log(const long &msg) {
-        std::ofstream fout("log.txt", std::ios::app);
-        fout << msg << std::endl;
-        fout.close();
-    }
-
-    void log(const unsigned long &msg) {
-        std::ofstream fout("log.txt", std::ios::app);
-        fout << msg << std::endl;
-        fout.close();
-    }
+    void readyShowLog(QString log);
 
 private:
-    Log() = default;
-    ~Log() = default;
-
+    static Log *log_obj;
 };
+
+#endif // LOG_H
--- backend/src/utils/log.cpp
+++ backend/src/utils/log.cpp
@@ -1,3 +1,20 @@
-//
-// Created by 李嘉政 on 2023/8/30.
-//
+#include "log.h"
+
+#include "Utilities/utilities.h"
+
+Log::Log(QObject *parent) : QObject(parent) {
+
+}
+
+Log *Log::log_obj = nullptr;
+
+Log *Log::GetLogObj() {
+    if (!log_obj) {
+        log_obj = new Log();
+    }
+    return log_obj;
+}
+
+void Log::writeLog(QString log_str) {
+    emit readyShowLog(StdDatetimeUtilities::GetDateTime() + " " + log_str);
+}
