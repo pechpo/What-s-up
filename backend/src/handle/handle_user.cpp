3f2a823a45f511e6c325a9cb4ee5bf44c9b12e57
hzxx066 <hzxx066@163.com>
2023-08-30 16:26:17 +0800

    update backend log for user

--- backend/src/handle/handle_user.cpp
+++ backend/src/handle/handle_user.cpp
@@ -8,6 +8,7 @@
 #include <QJsonArray>
 #include "handle.h"
 #include "db.h"
+#include "log.h"
 
 QJsonObject Handle::e_register(const int &ID, const QJsonObject &obj) {
     // Extract the necessary fields from obj
@@ -22,6 +23,11 @@ QJsonObject Handle::e_register(const int &ID, const QJsonObject &obj) {
     QJsonObject response;
     response["type"] = "r_register";
     response["success"] = flag;  // set to false if insertion fails
+    if (flag) {
+        writeLog("User Registration", "User with ID " + QString::number(id) + " registered successfully.", true);
+    } else {
+        writeLog("User Registration", "Failed to register user with ID " + QString::number(id) + ".", false);
+    }
     return response;
     // Send the response back to client
 }
@@ -36,6 +42,11 @@ QJsonObject Handle::q_login(const int &ID, const QJsonObject &obj) {
     QJsonObject response;
     response["type"] = "r_login";
     response["success"] = flag;  // set to false if query fails
+    if (flag) {
+        writeLog("User Login", "User with ID " + QString::number(id) + " logged in successfully.", true);
+    } else {
+        writeLog("User Login", "Failed to log in user with ID " + QString::number(id) + ".", false);
+    }
     return response;
     // Send the response back to client
 }
@@ -52,6 +63,7 @@ QJsonObject Handle::q_myInfo(const int &ID, const QJsonObject &obj) {
     response["password"] = flag.getPwd();
     response["email"] = flag.getEmail();
     response["avatar"] = flag.getAvatarName();
+    writeLog("Edit User Info", "User with ID " + QString::number(ID) + " edited their information.", true);
     return response;
     // Send the response back to client
 }
@@ -84,6 +96,7 @@ QJsonObject Handle::q_userInfo(const int &ID, const QJsonObject &obj) {
         }
     }
     response["sharedChats"] = sharedChats;
+    writeLog("Query Info", "User with ID " + QString::number(ID) + " queried information of user with ID " + QString::number(id) + ".", true);
     return response;
     // Send the response back to client
 }
@@ -105,6 +118,7 @@ QJsonObject Handle::e_editInfo(const int &ID, const QJsonObject &obj) {
     QJsonObject response;
     response["type"] = "r_editInfo";
     response["success"] = true;  // set to false if insertion fails
+    writeLog("Edit User Info", "User with ID " + QString::number(ID) + " edited their information.", true);
     return response;
     // Send the response back to client
 }
@@ -125,6 +139,7 @@ QJsonObject Handle::q_list_myChats(const int &ID, const QJsonObject &obj) {
         chats.append(chat);
     }
     response["chats"] = chats;
+    writeLog("Query Chat", "User with ID " + QString::number(ID) + " queried their chats.", true);
     return response;
     // Send the response back to client
 }
@@ -157,6 +172,11 @@ QJsonObject Handle::e_addFriend(const int &ID, const QJsonObject &obj) {
             y->sendMessage(OBJ);
         }
     }
+    if (flag) {
+        writeLog("Add Friend", "User with ID " + QString::number(ID) + " sent friend request to ID " + QString::number(id) + ".", true);
+    } else {
+        writeLog("Add Friend", "User with ID " + QString::number(ID) + " failed to send friend request to ID " + QString::number(id) + ".", false);
+    }
     return response;
     // Send the response back to client
 }
@@ -177,6 +197,7 @@ QJsonObject Handle::q_list_friendRequests(const int &ID, const QJsonObject &obj)
         users.append(user);
     }
     response["users"] = users;
+    writeLog("List Friend Requests", "User with ID " + QString::number(ID) + " listed their friend requests.", true);
     return response;
     // Send the response back to client
 }
@@ -197,6 +218,7 @@ QJsonObject Handle::q_list_myFriends(const int &ID, const QJsonObject &obj) {
         users.append(user);
     }
     response["users"] = users;
+    writeLog("List My Friends", "User with ID " + QString::number(ID) + " listed their friends.", true);
     return response;
     // Send the response back to client
 }
@@ -215,6 +237,12 @@ QJsonObject Handle::e_acceptFriend(const int &ID, const QJsonObject &obj) {
     if (!flag) {
         response["error"] = "Accept failed";
     }
+    if (flag) {
+        writeLog("Accept Friend", "User with ID " + QString::number(ID) + " accepted/rejected friend request from ID " + QString::number(id) + ".", true);
+    } else {
+        writeLog("Accept Friend", "User with ID " + QString::number(ID) + " failed to accept/reject friend request from ID " + QString::number(id) + ".", false);
+    }
+
     return response;
     // Send the response back to client
 }
